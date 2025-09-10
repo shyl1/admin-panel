e01371f1b0f52dd31896b0372991e6b61c540483
shyl1 <sohaylae615@gmail.com>
2025-09-10 22:41:50 +0300

    top box and mixin

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,20 @@
+import { Topbox } from '../../components/topbox/Topbox';
 import './home.scss';
 
 export const Home = () => {
     return (
-        <div className="home">Home</div>
+        <div className="home">
+            <div className="box box1">
+                <Topbox />
+            </div>
+            <div className="box box2">box2</div>
+            <div className="box box3">box3</div>
+            <div className="box box4">box4</div>
+            <div className="box box5">box5</div>
+            <div className="box box6">box6</div>
+            <div className="box box7">box7</div>
+            <div className="box box8">box8</div>
+            <div className="box box9">box9</div>
+        </div>
     )
 }
